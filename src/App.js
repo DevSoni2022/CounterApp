eac59513d157e1c00cf7cd44c3db011c741980ad
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2023-08-02 22:30:03 +0530

    Ui fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from "react";
+import "./App.css";
+class App extends Component {
+  inputRef = createRef();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super();
+    {
+      this.state = {
+        timer: 0,
+        start: false,
+        id: null,
+      };
+      this.incrementer = null;
+    }
+  }
+
+  handle = () => {
+    this.setState({
+      start: true,
+    });
+    this.incrementer = setInterval(() => {
+      return this.setState({
+        timer: this.state.timer + 1,
+      });
+    }, 1000);
+    this.setState({
+      id: null,
+    });
+    return () => {
+      clearInterval(this.incrementer);
+    };
+  };
+  Stop = () => {
+    debugger;
+    clearInterval(this.incrementer);
+  };
+  Reset = () => {};
+  Pause = () => {};
+
+  Reset = () => {
+    clearInterval(this.incrementer);
+    this.setState({
+      timer: 0,
+    });
+  };
+
+  render() {
+    console.log(this.inputRef, "$#@%@");
+    return (
+      <div className="App">
+        <div className="timer-count">{this.state.timer}</div>
+        <div className="btn-container">
+          <button style={{ margin: "5px" }} onClick={() => this.handle()}>
+            Start
+          </button>
+          <button style={{ margin: "5px" }} onClick={() => this.Stop()}>
+            Stop
+          </button>
+          <button style={{ margin: "5px" }} onClick={() => this.Reset()}>
+            Reset
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
